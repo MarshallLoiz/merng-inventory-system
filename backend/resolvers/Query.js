82e80c6c607ee849a54f2f1759a677fce5cf0418
Vanjo Loiz <akgamishanks01@gmail.com>
2021-04-25 01:39:39 +0800

    create store and stores resolver

--- backend/resolvers/Query.js
+++ backend/resolvers/Query.js
@@ -4,6 +4,21 @@ const Query = {
 
     return staffs
   },
+  async staff(parent, args, { Staff }, info) {
+    const staff = await Staff.findById(args.id)
+
+    return staff
+  },
+  async stores(parent, args, { Store }, info) {
+    const stores = await Store.find()
+
+    return stores
+  },
+  async store(parent, args, { Store }, info) {
+    const store = await Store.findById(args.id)
+
+    return store
+  },
 }
 
 module.exports = Query
