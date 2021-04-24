1ef50057424ada5835270e5196a732566aacd8ea
Vanjo Loiz <akgamishanks01@gmail.com>
2021-04-24 23:48:13 +0800

    update server file

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,24 @@
 const { GraphQLServer } = require('graphql-yoga')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+
+const Staff = require('./models/Staff')
+const Store = require('./models/Store')
+const Product = require('./models/Product')
+
 const connectDB = require('./config/db')
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`,
+const server = new GraphQLServer({
+  typeDefs: `${__dirname}/schema.graphql`,
+  resolvers: {
+    Query,
+    Mutation,
   },
-}
-
-const server = new GraphQLServer({ typeDefs, resolvers })
+  context: {
+    Staff,
+    Store,
+    Product,
+  },
+})
 
 server.start(connectDB())
