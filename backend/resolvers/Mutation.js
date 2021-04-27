599490f1f4bc076e91ba634b33bec3f5fbdc296b
Vanjo Loiz <akgamishanks01@gmail.com>
2021-04-27 19:52:11 +0800

    Create login mutations

--- backend/resolvers/Mutation.js
+++ backend/resolvers/Mutation.js
@@ -2,19 +2,49 @@ const generateToken = require('../utils/generateToken')
 const getUserId = require('../utils/getUserId')
 
 const Mutation = {
-  async createStaff(parent, { data }, { Staff, request }, info) {
-    getUserId(request)
-
-    const staff = await Staff.create(data)
-
-    return staff
-  },
   async createStore(parent, { data }, { Store }, info) {
     const store = await Store.create(data)
 
     return {
       store,
       token: generateToken(store._id),
+    }
+  },
+
+  async loginStore(parent, { data }, { Store }, info) {
+    const storeUser = await Store.findOne({ email: data.email })
+
+    if (storeUser && (await storeUser.matchPassword(data.password))) {
+      return {
+        store: storeUser,
+        token: generateToken(storeUser._id),
+      }
+    } else {
+      throw new Error('Invalid email or password')
+    }
+  },
+
+  async createStaff(parent, { data }, { Staff, request }, info) {
+    getUserId(request)
+
+    const staff = await Staff.create(data)
+
+    return {
+      staff,
+      token: generateToken(staff._id),
+    }
+  },
+
+  async loginStaff(parent, { data }, { Staff }, info) {
+    const staff = await Staff.findOne({ email: data.email })
+
+    if (staff && (await staff.matchPassword(data.password))) {
+      return {
+        staff,
+        token: generateToken(staff._id),
+      }
+    } else {
+      throw new Error('Invalid email or password')
     }
   },
 }
