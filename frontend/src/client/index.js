7629356004f635a8397400ef1d757785fb96004b
Vanjo Loiz <akgamishanks01@gmail.com>
2021-04-29 23:20:54 +0800

    Add credentials include

--- frontend/src/client/index.js
+++ frontend/src/client/index.js
@@ -3,4 +3,5 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 export const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
+  credentials: 'include',
 })
