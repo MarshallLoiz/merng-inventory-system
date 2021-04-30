2a3f83efea82cd66703b046fc51a0667ebc39d07
Vanjo Loiz <akgamishanks01@gmail.com>
2021-04-30 20:28:45 +0800

    Initial file of home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { ThemeProvider } from '@material-ui/core/styles'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './screens/Login/index'
+import Home from './screens/Home'
+
 import Header from './components/Header'
 
 import muiTheme from './theme/muiTheme'
@@ -13,7 +15,7 @@ const App = () => {
         <div className='app-container'>
           <main className='app-main'>
             <Switch>
-              <Route exact path='/' />
+              <Route exact path='/' component={Home} />
               <Route path='/login' component={Login} />
             </Switch>
           </main>
