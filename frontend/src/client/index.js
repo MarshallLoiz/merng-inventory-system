fe8bdae0d86edc65f25e8e36318357dc57af6d98
Vanjo Loiz <akgamishanks01@gmail.com>
2021-05-02 22:19:39 +0800

    Add authorization headers in apollo client

--- frontend/src/client/index.js
+++ frontend/src/client/index.js
@@ -1,7 +1,11 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
+import Cookies from 'js-cookie'
 
 export const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
   credentials: 'include',
+  headers: {
+    authorization: Cookies.get('jwt') || '',
+  },
 })
