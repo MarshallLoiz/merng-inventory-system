2c810a5105dccb683f19d28b8841fe9eed38f186
Vanjo Loiz <akgamishanks01@gmail.com>
2021-05-06 19:46:19 +0800

    Few tweaks in authorization config

--- frontend/src/client/index.js
+++ frontend/src/client/index.js
@@ -1,17 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import Cookies from 'js-cookie'
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
   credentials: 'include',
-  request: (operation) => {
-    const token = Cookies.get('jwt')
+})
 
-    operation.setContext({
-      headers: {
-        Authorization: token ? `Bearer ${token}` : '',
-      },
-    })
-  },
+const authLink = setContext((_, { headers }) => {
+  const token = Cookies.get('jwt')
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 })
