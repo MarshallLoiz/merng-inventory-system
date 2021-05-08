b964472015a85372fc6210aedf3e30ed7f73df4c
Vanjo Loiz <akgamishanks01@gmail.com>
2021-05-09 00:03:11 +0800

    Get the staffs data of the current login store

--- frontend/src/components/DashboardTable.js
+++ frontend/src/components/DashboardTable.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { useHistory, Link } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+import Button from '@material-ui/core/Button'
+import CircularProgress from '@material-ui/core/CircularProgress'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
@@ -7,26 +11,80 @@ import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
 import useStyles from '../jss/table'
+import AddIcon from '@material-ui/icons/Add'
+import { GET_STORE_STAFFS } from '../gql/Query'
 
 const DashboardTable = () => {
   const classes = useStyles()
 
+  const history = useHistory()
+
+  const handleCreateNewStaffButtonClick = () => {
+    history.push('/create-staff')
+  }
+
+  const { loading, data } = useQuery(GET_STORE_STAFFS, {
+    fetchPolicy: 'network-only',
+  })
+
+  const staffs = data ? data.staffs : []
+
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label='simple table'>
-        <TableHead>
-          <TableRow>
-            <TableCell className={classes.tableHeader}>First Name</TableCell>
-            <TableCell className={classes.tableHeader}>Last Name</TableCell>
-            <TableCell className={classes.tableHeader}>Gender</TableCell>
-            <TableCell className={classes.tableHeader}>Office Phone</TableCell>
-            <TableCell className={classes.tableHeader}>Job Title</TableCell>
-            <TableCell className={classes.tableHeader}></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody></TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <div className={classes.buttonWrapper}>
+            <Button
+              onClick={handleCreateNewStaffButtonClick}
+              className={classes.addNewStaffButton}
+              startIcon={<AddIcon />}
+            >
+              Add new Staff
+            </Button>
+          </div>
+          <TableContainer component={Paper}>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell className={classes.tableHeader}>
+                    First Name
+                  </TableCell>
+                  <TableCell className={classes.tableHeader}>
+                    Last Name
+                  </TableCell>
+                  <TableCell className={classes.tableHeader}>Gender</TableCell>
+                  <TableCell className={classes.tableHeader}>
+                    Office Phone
+                  </TableCell>
+                  <TableCell className={classes.tableHeader}>
+                    Job Title
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {staffs.map((staff) => (
+                  <TableRow
+                    className={classes.tableBodyRow}
+                    component={Link}
+                    to={`/staff/${staff.id}`}
+                    key={staff.id}
+                    hover
+                  >
+                    <TableCell>{staff.firstName}</TableCell>
+                    <TableCell>{staff.lastName}</TableCell>
+                    <TableCell>{staff.gender}</TableCell>
+                    <TableCell>{staff.officePhone}</TableCell>
+                    <TableCell>{staff.jobTitle}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
+    </>
   )
 }
 
--- frontend/src/gql/Query.js
+++ frontend/src/gql/Query.js
@@ -7,3 +7,19 @@ export const GET_CURRENT_STORE_LOGIN_USER_FOR_HEADER = gql`
     }
   }
 `
+
+export const GET_STORE_STAFFS = gql`
+  query GetStoreStaffs {
+    staffs {
+      id
+      firstName
+      lastName
+      gender
+      officePhone
+      email
+      dateOfJoin
+      dateOfBirth
+      jobTitle
+    }
+  }
+`
