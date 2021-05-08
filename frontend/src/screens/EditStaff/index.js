5ac8019d2554da7e3adc250dff3022c7a97740a8
Vanjo Loiz <akgamishanks01@gmail.com>
2021-05-09 00:04:15 +0800

    Initial Files

---
+++ frontend/src/screens/EditStaff/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+const EditStaff = () => {
+  return (
+    <div>
+      <Header />
+      Hello from Edit staff screen
+    </div>
+  )
+}
+
+export default EditStaff
