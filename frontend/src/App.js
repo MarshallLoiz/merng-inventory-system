53d619c837e2cb9ee21148f39d98ac3961efbbd1
Vanjo Loiz <akgamishanks01@gmail.com>
2021-05-09 00:02:23 +0800

    Add create and edit screen in app js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from './screens/Login/index'
 import Home from './screens/Home'
 import Register from './screens/Register'
+import EditStaff from './screens/EditStaff'
+import CreateStaff from './screens/CreateStaff'
 
 import muiTheme from './theme/muiTheme'
 
@@ -16,6 +18,8 @@ const App = () => {
               <Route exact path='/' component={Home} />
               <Route path='/login' component={Login} />
               <Route path='/register' component={Register} />
+              <Route path='/staff/:id' component={EditStaff} />
+              <Route path='/create-staff' component={CreateStaff} />
             </Switch>
           </main>
         </div>
