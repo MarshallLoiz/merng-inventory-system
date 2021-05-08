2965e225f421925bf2457ccde7570c83b21993ba
Vanjo Loiz <akgamishanks01@gmail.com>
2021-05-09 00:04:01 +0800

    Table styles

--- frontend/src/jss/table.js
+++ frontend/src/jss/table.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   table: {
     minWidth: 650,
   },
@@ -8,6 +8,22 @@ const useStyles = makeStyles({
     fontWeight: 'bold',
     fontSize: '16px',
   },
-})
+  tableBodyRow: {
+    cursor: 'pointer',
+    textDecoration: 'none',
+  },
+  buttonWrapper: {
+    display: 'flex',
+    marginBottom: '20px',
+  },
+  addNewStaffButton: {
+    justifyContent: 'flex-end',
+    marginLeft: 'auto',
+    backgroundColor: theme.palette.button.backgroundColor,
+    '&:hover': {
+      backgroundColor: theme.palette.button.backGroundColorhover,
+    },
+  },
+}))
 
 export default useStyles
